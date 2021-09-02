a1bfcc6680b29f8e7b3a3a849d7c9fe302944bfd
hchtut <hch.htut.yours@gmail.com>
2021-09-03 01:25:37 +0630

    Finished Project

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let usernameInput = document.getElementById('userNameInput')
-usernameInput.addEventListener('keyup', (event) => {
-    let username = event.target.value.toLowerCase();
-    console.log(username)
+let searchInput = document.getElementById('searchInput')
+
+searchInput.addEventListener('keyup', (event) => {
+    let searchQuery = event.target.value.toLowerCase();
+    let allNameDOMcollections = document.getElementsByClassName('name')
+    for (let counter = 0; counter < allNameDOMcollections.length; counter++) {
+        let currentName = allNameDOMcollections[counter].textContent.toLowerCase()
+
+        if (currentName.includes(searchQuery)) {
+            allNameDOMcollections[counter].style.display = 'block'
+        } else {
+            allNameDOMcollections[counter].style.display = 'none'
+        }
+    }
 })
